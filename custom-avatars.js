79e601e4240284c8960aa6191ff3d6ce547ad7f0
rkdrjsdnwkds <rkdrjsdnwkds@gmail.com>
2026-05-21 14:13:17 +0900

    Add six custom avatars

--- custom-avatars.js
+++ custom-avatars.js
@@ -6,5 +6,11 @@ export const CUSTOM_AVATARS = {
   custom_devilgu:{id:"custom_devilgu",name:"데빌구",type:"avatar",icon:"▣",price:300,rarity:"희귀",src:"assets/avatars/custom_devilgu.png"},
   custom_wootan:{id:"custom_wootan",name:"작은 우탄",type:"avatar",icon:"▣",price:300,rarity:"희귀",src:"assets/avatars/custom_wootan.png"},
   custom_octopus:{id:"custom_octopus",name:"문어",type:"avatar",icon:"▣",price:300,rarity:"희귀",src:"assets/avatars/custom_octopus.png"},
-  custom_mario:{id:"custom_mario",name:"매뤼오",type:"avatar",icon:"▣",price:300,rarity:"희귀",src:"assets/avatars/custom_mario.png"}
+  custom_mario:{id:"custom_mario",name:"매뤼오",type:"avatar",icon:"▣",price:300,rarity:"희귀",src:"assets/avatars/custom_mario.png"},
+  custom_nyangnyangnyanyang:{id:"custom_nyangnyangnyanyang",name:"냥냥냐냥",type:"avatar",icon:"▣",price:300,rarity:"희귀",src:"assets/avatars/custom_nyangnyangnyanyang.png"},
+  custom_pworworo:{id:"custom_pworworo",name:"풔뤄뤄",type:"avatar",icon:"▣",price:300,rarity:"희귀",src:"assets/avatars/custom_pworworo.png"},
+  custom_question:{id:"custom_question",name:"?",type:"avatar",icon:"▣",price:300,rarity:"희귀",src:"assets/avatars/custom_question.png"},
+  custom_cheongchun:{id:"custom_cheongchun",name:"청춘뭐시기",type:"avatar",icon:"▣",price:300,rarity:"희귀",src:"assets/avatars/custom_cheongchun.png"},
+  custom_twokkwi:{id:"custom_twokkwi",name:"퉈뀌",type:"avatar",icon:"▣",price:300,rarity:"희귀",src:"assets/avatars/custom_twokkwi.png"},
+  custom_surisuri:{id:"custom_surisuri",name:"수리수리주수리",type:"avatar",icon:"▣",price:300,rarity:"희귀",src:"assets/avatars/custom_surisuri.png"}
 };
